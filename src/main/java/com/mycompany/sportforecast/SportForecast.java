fb12e798f0c86524e9bcc51b543644c9f22bdc1a
Javier Lopez <javierlopez_85@hotmail.com>
2023-03-14 19:31:42 -0300

    initial commit

---
+++ src/main/java/com/mycompany/sportforecast/SportForecast.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sportforecast;
+
+import tp.TP;
+/**
+ *
+ * @author javierl
+ */
+
+/**
+ *
+ * Este trabajo esta realizado en colaboracion por:
+ * - Ezequiel Criscueli - criscuelieduardo@gmail.com
+ * - Daniela Bastias - danybastias@outlook.com
+ * - Ricardo jorge herrera - Ricardherrera35@gmail.com 
+ * - Melina Díaz - melinaydiaz@gmail.com
+ * - Javier Lopez - jglopezr.018@gmail.com
+ */
+
+public class SportForecast {
+    private static TP tp;
+    public static void main(String[] args) {
+        tp = new TP();
+        
+        tp.executeMain();   //Executes mainly funtion of TP class imported here
+    }
+}
