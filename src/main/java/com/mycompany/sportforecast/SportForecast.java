6e602f650ad64e90ea88d87dc2265dc77bb9fc67
Javier Lopez <javierlopez_85@hotmail.com>
2023-04-18 16:14:05 -0300

    winners messages added, version changed on pom.xml

--- src/main/java/com/mycompany/sportforecast/SportForecast.java
+++ src/main/java/com/mycompany/sportforecast/SportForecast.java
@@ -5,13 +5,15 @@
 package com.mycompany.sportforecast;
 
 import de.vandermeer.asciitable.AsciiTable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * Este trabajo esta realizado en colaboracion por:
  * - Ezequiel Criscueli - criscuelieduardo@gmail.com
  * - Daniela Bastias - danybastias@outlook.com
- * - Ricardo jorge herrera - Ricardherrera35@gmail.com 
+ * - Ricardo Jorge Herrera - Ricardherrera35@gmail.com 
  * - Melina Díaz - melinaydiaz@gmail.com
  * - Javier Lopez - jglopezr.018@gmail.com
  */
@@ -25,20 +27,50 @@ public class SportForecast {
     
     public static void play(ListaParticipantes participantes) {
         AsciiTable atable = new AsciiTable();
+        List<Participante> winners = new ArrayList<>();
+        boolean firstLoop = true;
+        int maxScore = 0;
         
         atable.addRule();
         atable.addRow("Nombre", "puntos");
         atable.addRule();
         
         for(Participante participante : participantes.getSortedParticipantes()) {
+            int participanteScore = participante.getScorePronostico();
             atable.addRow(
                     participante.getNombre().replaceAll("\"", ""),
-                    participante.getScorePronostico()
+                    participanteScore
             );
             atable.addRule();
+            
+            if(firstLoop) {
+                firstLoop = false;
+                maxScore = participanteScore;
+            }
+            
+            if(maxScore == 0 || maxScore == participanteScore) {
+                winners.add(participante);
+            }
         }
         
         System.out.print(atable.render());
+        
+        if(winners.size() >= 2) {
+            System.out.println("\n >>> Ganadoras(es): ");
+            winners.forEach(elem -> {
+                System.out.println(
+                    "\t * " + elem.getNombre().replace("\"", "")
+                );
+            });
+        } else if (winners.size() == 1){
+            System.out.println(
+                ">>> Ganador(a): \n" +
+                winners.get(0).getNombre().replace("\"", "") + 
+                "!!!"
+            );
+        } else {
+            System.out.println(">>> No hubo Ganador!!! <<<");
+        }
     }
 }
 
